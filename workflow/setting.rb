e100b4568ed2ea49c8e7fed32e1db0619dcc3131
Shin Tokiwa <shin.tokiwa@zealot.co.jp>
2014-02-18 16:44:31 +0900

    Improve account input and fix setting file

---
+++ workflow/setting.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require "alfred"
+
+class Setting
+  def initialize(alfred)
+    @setting ||= Alfred::Setting.new alfred
+    @setting[:cookie_yaml] = 'cookie.yaml'
+    @setting[:cache_expiration] = 300
+    @setting[:base_url] = 'https://i.doit.im'
+    @setting.dump
+  end
+
+  def get(key)
+    begin
+      @setting[key.to_sym]
+    rescue NameError => ex
+      generate alfred
+      get alfred, key
+    end
+  end
+
+  def set(key, value)
+    begin
+      @setting[key.to_sym] = value
+      @setting.dump
+    rescue NameError => ex
+      generate alfred
+      set alfred, key, value
+    end
+  end
+end
--- workflow/util.rb
+++ workflow/util.rb
@@ -3,29 +3,50 @@
 
 require "alfred"
 require "mechanize"
+require "./setting"
 
 class Util
+  def self.prompt_username
+    username = %x[osascript -e 'display dialog "Username" default answer ""' -e 'text returned of result']
+    username.chomp!
+    raise if username.empty?
+    return username
+  end
+
+  def self.prompt_password
+    password = %x[osascript -e 'display dialog "Password" default answer "" with hidden answer' -e 'text returned of result']
+    password.chomp!
+    raise if password.empty?
+    return password
+  end
+
   def self.get_enabled_agent
     agent = Mechanize.new
 
-    if File.exists? "./cookie.yaml" # FIXME
+    if File.exists? @@cookie_yaml
       # Load cookie from file if exist
-      agent.cookie_jar.load "./cookie.yaml" # FIXME
+      agent.cookie_jar.load @@cookie_yaml
     else
       # Create a session newly
-      page = agent.get "https://i.doit.im/signin"
+      page = agent.get "#{@@setting.get(:base_url)}/signin"
       form = page.forms[0]
-      form.username = "" # FIXME
-      form.password = "" # FIXME
+      form.username = prompt_username
+      form.password = prompt_password
       agent.submit form
-      agent.cookie_jar.save_as "./cookie.yaml" # FIXME
+      agent.cookie_jar.save_as @@cookie_yaml
     end
     return agent
   end
 
   def self.get_tasks_as_json(attribute)
     agent = self.get_enabled_agent
-    page = agent.get "https://i.doit.im/api/tasks/#{attribute}"
+    begin
+      page = agent.get "#{@@setting.get(:base_url)}/api/tasks/#{attribute}"
+    rescue
+      # Retry to get an enabled agent when any exception occurs or HTTP status is 401
+      File.delete @@cookie_yaml
+      get_tasks_as_json attribute
+    end
     JSON.parse page.body
   end
 
@@ -44,15 +65,14 @@ class Util
 
   def self.exec(attribute = "today")
     Alfred.with_friendly_error do |alfred|
+      @@setting ||= Setting.new alfred
+      @@cookie_yaml ||= "#{alfred.storage_path}/#{@@setting.get(:cookie_yaml)}"
+
       # Enabling cache (https://github.com/zhaocai/alfred-workflow#automate-saving-and-loading-cached-feedback)
       alfred.with_rescue_feedback = true
       alfred.with_cached_feedback do
-        use_cache_file :expire => 300
+        use_cache_file :expire => @@setting.get(:cache_expiration)
       end
-
-      # setting = Alfred::Setting.new alfred
-      # setting[:base_url] = 'https://i.doit.im'
-      # setting.dump
 
       if fb = alfred.feedback.get_cached_feedback
         puts fb.to_alfred(ARGV)
